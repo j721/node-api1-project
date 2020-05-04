de40628ff812e90c825a15ab9602c3290f41df89
j721 <juliadvu@gmail.com>
2020-05-04 18:26:56 -0500

    got post endpoint working

--- index.js
+++ index.js
@@ -1,58 +1,79 @@
-const express = require ("express");
+const express = require("express");
 const shortid = require("shortid");
-const server = express(); 
+const server = express();
 
 server.use(express.json());
 
-let users =[{
-    id: shortid.generate(), 
+let users = [
+  {
+    id: shortid.generate(),
     name: "Joe Smith",
-    bio: "Frontend Developer"
-},
-{
-    id: shortid.generate(), 
+    bio: "Frontend Developer",
+  },
+  {
+    id: shortid.generate(),
     name: "Violet Rose",
-    bio: "UX Designer"
-},
-{
-    id: shortid.generate(),  
+    bio: "UX Designer",
+  },
+  {
+    id: shortid.generate(),
     name: "Mark Red",
-    bio: "Backend Developer"
-}
+    bio: "Backend Developer",
+  },
 ];
 
-function getUsers(){
-    return users; 
+function getUsers() {
+  return users;
 }
 
-function getUserById(id){
-    return users.find((user)=>user.id ===id )
+function getUserById(id) {
+  return users.find((user) => user.id === id);
 }
 
-server.get("/", (req, res)=>{
-    res.json({api: "api is working!" })
-})
+server.get("/", (req, res) => {
+  res.json({ api: "api is working!" });
+});
 
-server.get("/api/users", function (req, res){
-    //returns an array of users (id, name, bio)
-    res.json(users);
-})
+server.get("/api/users", function (req, res) {
+  //returns an array of users (id, name, bio)
+  res.json(users);
+});
 
-server.post("/api/users", function (req, res){
-    const userInformation = req.body;
+function createUser(data) {
+  const payload = {
+    id: shortid.generate(),
+    ...data,
+  };
 
-    users.push(userInformation);
+  users.push(payload);
+  return payload;
+}
 
-    res.status(201).json(userInformation)
+server.post("/api/users", function (req, res) {
 
-})
+    if(!req.body.name || !req.body.bio){
+        res.status(400).json({errorMessage: "Please provide name and bio"})
+    }
 
-server.delete("/api/users/:id", function (req, res){
-    const id = Number(req.params.id);
+  const userInformation = createUser({
+        id: shortid.generate(), 
+        name: req.body.name,
+      bio: req.body.bio,
+  })
+  
+//   req.body;
 
-    users = users.filter(user=>user.id !==id)
+  users.push(userInformation);
 
-    res.status(200).json(users);
-})
+  res.status(201).json(userInformation);
+});
 
-server.listen (8000, ()=>console.log("\n==API is up==\n"))
+server.delete("/api/users/:id", function (req, res) {
+  const id = Number(req.params.id);
+
+  users = users.filter((user) => user.id !== id);
+
+  res.status(200).json(users);
+});
+
+server.listen(8000, () => console.log("\n==API is up==\n"));
