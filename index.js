8d48ce0a10d2136d93f35274aad955faf2dcf773
j721 <juliadvu@gmail.com>
2020-05-04 16:42:23 -0500

    working on delete endpoint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require ("express");
-
+const db = require('./database');
 const server = express(); 
 
 server.use(express.json());
@@ -16,8 +16,24 @@ server.get("/", (req, res)=>{
 
 server.get("/api/users", function (req, res){
     //returns an array of users (id, name, bio)
-
     res.json(users);
+})
+
+server.post("/api/users", function (req, res){
+    const userInformation = req.body;
+
+    users.push(userInformation);
+
+    res.status(201).json(userInformation)
+
+})
+
+server.delete("/api/users/:id", function (req, res){
+    const id = Number(req.params.id);
+
+    users = users.filter(user=>user.id !==id)
+
+    res.status(200).json(users);
 })
 
 server.listen (8000, ()=>console.log("\n==API is up==\n"))
